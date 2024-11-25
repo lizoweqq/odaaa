6699a428311ff2c42158017b851075e8591fd590
Your Name <you@example.com>
2024-11-25 20:08:55 +0200

    add more files

---
+++ main.py
@@ -0,0 +1,14 @@
+# модуль
+import poems
+import sign
+
+
+# змінна
+author_name = "ліза мороз"
+
+# формат
+print(sign.ODA_LIZA.format(author_name))
+
+# любий текст
+wishes = "успіхів усім!\n мені також!"
+print(wishes)
