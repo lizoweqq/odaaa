4c7a56e0889c74deaea0f9851adbfc6fb69d018c
Your Name <you@example.com>
2024-12-12 13:17:18 +0200

    ok_price.py

--- ok_price.py
+++ ok_price.py
@@ -8,7 +8,7 @@ def zoo_ticket_price():
     # запит віку
     age = input("будь ласка, введіть ваш вік:", type=NUMBER)
 
-    # Обчислення ціни
+    # обчислення ціни
     if age < 6:  # до 6 ьезкоштовно
         price = "безкоштовно"
     elif 6 <= age < 13:  # від 6 до 12 знижка 50%
@@ -20,7 +20,7 @@ def zoo_ticket_price():
     else:  # більше 60 знижка 30%
         price = f"{int(BASE_PRICE * 0.7)} грн (знижка 30%)"
 
-    put_text(f"Вартість квитка: {price}")
+    put_text(f"вартість квитка: {price}")
 
 # результат
 if __name__ == "__main__":
